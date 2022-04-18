19d595892f07b9ac754524765762b596de3dbb64
Isabela <49378594+IsabelaEtt@users.noreply.github.com>
2022-04-18 08:23:52 -0300

    first commit

---
+++ src/monitor/Monitor.java
@@ -0,0 +1,9 @@
+package monitor;
+
+// @author Isabela Canelas Ett - RA00303107
+
+public class Monitor {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
